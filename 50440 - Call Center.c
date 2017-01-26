7e7b64c7c34b5d5b3f3e1fc38778dae216a06ce0
fskuka <skuka.f@gmail.com>
2017-01-27 00:14:23 +0100

    Update 50440 - Call Center.c

--- 50440 - Call Center.c
+++ 50440 - Call Center.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+int calcAmount(int talkTime);
 
 int main()
 {
-    FILE *infile;
-    infile=fopen("input.txt","r");
-    if(infile==NULL)
+    FILE *fptr;
+    fptr=fopen("input.txt","r");
+    if(fptr==NULL)
         exit(1);
 
-    int number;
-    char ch;
-    do
+    int phCalls, talkTime,i ;
+    int clientAmount, totAmount=0 ;
+
+    fscanf(fptr,"%d",&phCalls);
+
+    for (i=1;i<=phCalls;i++)
     {
-        fscanf(infile,"%d%c",&number,&ch);
-        printf("%d%c",number,ch);
-    }while(ch!=';');
+        fscanf(fptr,"%d",&talkTime);
+        clientAmount=calcAmount(talkTime);
+        totAmount+=clientAmount;
+    }
 
-    fclose(infile);
+    printf("%d",totAmount);
 
+    fclose(fptr);
     return 0;
+}
+
+
+
+int calcAmount(int talkTime)
+{
+    int amount=0;
+    int additSec;
+
+    if(talkTime<=120)
+         amount=100; /* 100 lek */
+    else
+        {
+         amount+=100;
+         additSec=talkTime-120;
+         amount+=ceil((float)additSec/10)*5;
+        }
+    return amount;
 }
